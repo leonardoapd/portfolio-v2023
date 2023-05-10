d5233443a18f271d912c6539cacec9c14fcee517
Leonardo Perdomo <leonardop763@gmail.com>
2023-05-10 02:27:54 -0400

    Skills tooltip added

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,12 +1,30 @@
 /* eslint-disable react-refresh/only-export-components */
 import AppWrapper from "../../wrapper/AppWrapper";
 import SkillsInformation from '../../assets/data/data.json';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 import './Skills.css';
 
 
 function Skills() {
 
     const skills = SkillsInformation.skills;
+
+    const renderTooltip = (name) => (
+        <Tooltip
+            id="button-tooltip"
+            style={
+                {
+                    backgroundColor: 'var(--primary-color)',
+                    color: 'white',
+                    borderRadius: 3,
+                    padding: '2px 10px',
+                }
+            }
+        >
+            {name}
+        </Tooltip>
+    );
 
     return (
         <>
@@ -17,7 +35,13 @@ function Skills() {
                 {
                     skills.map((skill, index) => (
                         <figure className="skill_container" key={index}>
-                            <img src={skill.image} alt="" className="skill_img"></img>
+                            <OverlayTrigger
+                                placement="bottom"
+                                delay={{ show: 250, hide: 400 }}
+                                overlay={renderTooltip(skill.name)}
+                            >
+                                <img src={skill.image} alt="" className="skill_img"></img>
+                            </OverlayTrigger>
                             {/* <figcaption className="skill_name">{skill.name}</figcaption> */}
                         </figure>
                     ))
