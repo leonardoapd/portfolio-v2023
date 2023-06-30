4db4c4bd81a77159a32964a1edced2158c0fcaaf
Leonardo Perdomo <leonardop763@gmail.com>
2023-06-30 15:59:18 -0400

    Fix (NavigationDots): Minor bugs fixed with the background color in the dots

--- src/components/NavigationDots/NavigationDots.jsx
+++ src/components/NavigationDots/NavigationDots.jsx
@@ -2,11 +2,11 @@ import './NavigationDots.css';
 
 export default function NavigationDots({active}) {
     return (
-        <div className="app__navigation">
+        <div className="app__navigation ">
             {['about', 'projects', 'skills', 'testimonials'].map((item, index) => (
                 <a href={`#${item}`}
                     key={index}
-                    className="app__navigation-dot"
+                    className="app__navigation-dot navdot_background"
                     style={active === item ? { backgroundColor: '#4e56d3' } : {}}
                 />
             ))}
