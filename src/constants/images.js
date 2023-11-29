12616c3fb915f052358ef4b3c0160f72ee248ee9
Leonardo Perdomo <leonardop763@gmail.com>
2023-11-28 23:55:57 -0500

    Refactor: skills sections layout and info changed.

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,12 @@
-import online_page from '../assets/images/images/online-page.png';
+const images = {};
 
-export const images = {
-    online_page,
-};
+// Import all images in ../assets/images/skills/*.png or svg
+const imageContext = import.meta.globEager('../assets/images/skills/*.png');
+
+// Add all images to images object
+Object.keys(imageContext).forEach((image) => {
+    const imageName = image.split('/').pop().split('.')[0];
+    images[imageName] = imageContext[image].default;
+});
+
+export default images;
